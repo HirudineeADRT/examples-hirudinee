d177f45e08879357afa0f203d63f24d66025d663
HirudineeADRT <hirudinee@adroitlogic.com>
2019-09-02 14:58:48 +0530

    added new lambda

--- serve-dynamic-html-via-http-endpoint/handler.js
+++ serve-dynamic-html-via-http-endpoint/handler.js
@@ -15,8 +15,19 @@ module.exports.landingPage = (event, context, callback) => {
     <body>
       <h1>Landing Page</h1>
       ${dynamicHtml}
+ <form action=${testaction}>
+  First name:<br>
+  <input type="text" name="firstname" value="Mickey">
+  <br>
+  Last name:<br>
+  <input type="text" name="lastname" value="Mouse">
+  <br><br>
+  <input type="submit" value="Submit">
+</form>
     </body>
   </html>`;
+
+  console.log(testaction);
 
   const response = {
     statusCode: 200,
